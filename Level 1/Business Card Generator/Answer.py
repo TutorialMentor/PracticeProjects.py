f83c3751b3cf11bb6a0c450fc05340fc8723b6f8
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-07 11:47:48 -0600

    Create Answer.py

---
+++ Level 1/Business Card Generator/Answer.py
@@ -0,0 +1,9 @@
+name = "Tooti Patooti"
+job_title = "Software Developer"
+email = "Tooti.patooti@example.com"
+
+print("Business Card:")
+print("-----------------")
+print("Name: " + name)
+print("Job Title: " + job_title)
+print("Email: " + email)
