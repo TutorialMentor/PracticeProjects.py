e6b1b3ce7d54b9fe64054c3cff45c0e130dcd6a1
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 16:30:45 -0600

    Create Answer.py

---
+++ Level 1/Simple FAQ Page/Answer.py
@@ -0,0 +1,12 @@
+question1 = "What is Python?"
+answer1 = "Python is a programming language."
+
+question2 = "Why learn Python?"
+answer2 = "It is versatile and beginner-friendly."
+
+print("FAQs:")
+print(question1)
+print(answer1)
+print()
+print(question2)
+print(answer2)
