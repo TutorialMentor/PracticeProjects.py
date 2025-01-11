34516968197d91b93687e24a80ba1c49ef7e01f8
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 16:22:36 -0600

    Create Answer.py

---
+++ Level 1/Event Reminder/Answer.py
@@ -0,0 +1,10 @@
+event_name = "Team Meeting"
+date = "January 10, 2025"
+time = "11:00 AM"
+location = "Conference Room A"
+
+print("Event Reminder:")
+print("Event: " + event_name)
+print("Date: " + date)
+print("Time: " + time)
+print("Location: " + location)
