3e96ed3db8ac449eeb7e4b8823d5ac31d1b2af89
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 16:36:21 -0600

    Create Answer.py

---
+++ Level 1/Product Advertisement/Answer.py
@@ -0,0 +1,9 @@
+product_name = "Smartphone X"
+price = "$799"
+features = "- High-resolution camera\n- Long-lasting battery\n- Fast processor"
+
+print("Product Advertisement:")
+print("Introducing the new " + product_name + "!")
+print("Price: " + price)
+print("Features:")
+print(features)
