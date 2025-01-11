f3d9ca84be2ff1b3885a8023eee3013fdfcbb8ae
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 17:03:57 -0600

    Create Answer.py

---
+++ Level 2/User Profile Display/Answer.py
@@ -0,0 +1,10 @@
+username = "alice_wonder"
+age = 28
+height = 5.6
+is_student = False
+
+print("User Profile")
+print("Username: " + username)
+print("Age: " + str(age))
+print("Height (in feet): " + str(height))
+print("Is a student: " + str(is_student))
