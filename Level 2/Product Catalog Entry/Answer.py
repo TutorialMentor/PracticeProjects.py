f633e2cdaa0edd7d7279f6f53746ccd0d7ab7e1d
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 23:11:54 -0600

    Create Answer.py

---
+++ Level 2/Product Catalog Entry/Answer.py
@@ -0,0 +1,10 @@
+product_name = "Apple"
+product_id = 345683544
+price = 1.50
+in_stock = True
+
+print("Product Information")
+print("Name:" + product_name)
+print("ID:" + str(product_id))
+print("Price:" + str(price))
+print("Available:" + str(in_stock))
