5098f7e5739da5c10ea87be8f76d425a5464cbf9
Tutorial Mentor <30102373+TutorialMentor@users.noreply.github.com>
2025-01-11 23:18:00 -0600

    Create Answer.py

---
+++ Level 2/Weather Report/Answer.py
@@ -0,0 +1,9 @@
+city = "Toronto"
+temperature = 46.32
+humidity = 13
+is_raining = False
+
+print("Weather Report for " + city)
+print("Temperature: " + str(temperature))
+print("Humidity: " + str(humidity))
+print("Raining: " + str(is_raining))
